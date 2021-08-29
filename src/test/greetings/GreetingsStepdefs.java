f927f6d8e63586de9e79f0a20b0999da939ad27f
Ali Koptan <33788749+alikoptan@users.noreply.github.com>
2021-08-29 14:43:02 +0200

    Update GreetingsStepdefs.java

--- src/test/greetings/GreetingsStepdefs.java
+++ src/test/greetings/GreetingsStepdefs.java
@@ -2,6 +2,11 @@ package test.greetings;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GreetingsStepdefs {
 
@@ -13,5 +18,19 @@ public class GreetingsStepdefs {
     @And("Your name should be {string}")
     public void yourNameShouldBe(String arg0) {
         System.out.println("Your name is " + arg0);
+    }
+
+    @And("I visit {string}")
+    public void iVisit(String url) {
+        String chromeDriverPath = "chromedriver";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get(url);
+        WebElement element = driver.findElement(By.tagName("html"));
+        String elementSource = element.getAttribute("innerHTML");
+        System.out.println(elementSource);
+        driver.close();
     }
 }
